70a395504e19d4be3a191b84b7d41e75c7a0028c
gbouwen <gijsbouwen@gmail.com>
2022-04-22 17:55:24 +0200

    Revert "Remove unused request"
    This reverts commit 50e689ff8c2f7469b62e56c11a41aaabbc536980.

--- app/src/main/java/com/standings/android/api/FootballStandingsApi.kt
+++ app/src/main/java/com/standings/android/api/FootballStandingsApi.kt
@@ -1,7 +1,9 @@
 package com.standings.android.api
 
 import com.standings.android.model.AllLeagues
+import com.standings.android.model.League
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface FootballStandingsApi {
 
@@ -11,5 +13,8 @@ interface FootballStandingsApi {
 
     @GET("leagues")
     suspend fun getLeagues(): AllLeagues
+
+    @GET("leagues/{id}")
+    suspend fun getLeague(@Path("id") id: String): League
 
 }
--- app/src/main/java/com/standings/android/repository/Repository.kt
+++ app/src/main/java/com/standings/android/repository/Repository.kt
@@ -2,9 +2,12 @@ package com.standings.android.repository
 
 import com.standings.android.api.RetrofitInstance
 import com.standings.android.model.AllLeagues
+import com.standings.android.model.League
 
 class Repository {
 
     suspend fun getLeagues(): AllLeagues = RetrofitInstance.api.getLeagues()
+
+    suspend fun getLeague(id: String): League = RetrofitInstance.api.getLeague(id)
 
 }
