aa13f456adc2434a41ae4ad887792449aa65488d
gbouwen <gijsbouwen@gmail.com>
2022-04-26 13:03:42 +0200

    Add Logging Interceptor

--- app/src/main/java/com/standings/android/singletons/RetrofitInstance.kt
+++ app/src/main/java/com/standings/android/singletons/RetrofitInstance.kt
@@ -2,15 +2,28 @@ package com.standings.android.singletons
 
 import com.standings.android.api.FootballStandingsApi
 import com.standings.android.api.FootballStandingsApi.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitInstance {
 
+    private val loggingInterceptor by lazy {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
